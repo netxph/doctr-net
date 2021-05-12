00878869bfaf1815e671738989c6694e98ff9570
Marc Vitalis <marcabraham.vitalis@amadeus.com>
2021-05-12 23:37:08 +0800

    initial commit

---
+++ src/Doctr.Diagnostics/LoggerTraceListener.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace Doctr.Diagnostics
+{
+
+    public class LoggerTraceListener : TraceListener
+    {
+        private readonly ILogger logger;
+
+        public LoggerTraceListener(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public override void Write(string message)
+        {
+            var method = GetCallingMethod();
+
+            logger.LogTrace($"[{method.DeclaringType.Name}::{method.Name}]: {message}");
+        }
+
+        public override void WriteLine(string message)
+        {
+            var method = GetCallingMethod();
+
+            logger.LogTrace($"[{method.DeclaringType.Name}::{method.Name}]: {message}");
+        }
+
+        public override void WriteLine(string message, string category)
+        {
+            var method = GetCallingMethod();
+
+            logger.LogTrace($"[{method.DeclaringType.Name}::{method.Name}]: {category}:{message}");
+        }
+
+        protected virtual MethodBase GetCallingMethod()
+        {
+            var depth = 3;
+
+            var method = new StackTrace().GetFrame(depth).GetMethod();
+
+            while(method.DeclaringType == typeof(LoggerTraceListener) ||
+                    method.DeclaringType == typeof(Trace) ||
+                    method.DeclaringType.Name.Equals("TraceInternal"))  
+            {
+                depth++;
+                method = new StackTrace().GetFrame(depth).GetMethod();
+            }
+
+            return method;
+        }
+
+    }
+
+}
